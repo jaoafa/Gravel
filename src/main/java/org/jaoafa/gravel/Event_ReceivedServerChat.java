f4e0ef6cadc2153e00df4ec0d0f621939e5a2122
Tomachi [ICHIGO] <lego@aol.jp>
2021-03-22 02:10:48 +0900

    init

---
+++ src/main/java/org/jaoafa/gravel/Event_ReceivedServerChat.java
@@ -0,0 +1,74 @@
+package org.jaoafa.gravel;
+
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class Event_ReceivedServerChat implements Listener {
+    @SubscribeEvent
+    public void onReadyEvent(ReadyEvent event) {
+        Bukkit.getServer().sendMessage(Component.text().append(
+            Component.text("[Gravel]"),
+            Component.space(),
+            Component.text("ログイン完了:"),
+            Component.space(),
+            Component.text(event.getJDA().getSelfUser().getAsTag(), NamedTextColor.GREEN)
+        ));
+    }
+
+    @SubscribeEvent
+    public void onReceived(MessageReceivedEvent event) {
+        if(event.getChannel().getIdLong() != 823229253637373962L){
+            return;
+        }
+        if(event.getAuthor().getIdLong() == event.getJDA().getSelfUser().getIdLong()){
+            return;
+        }
+        Bukkit.getServer().sendMessage(Component.text().append(
+            Component.text("[Discord]", NamedTextColor.AQUA),
+            Component.space(),
+            Component.text(event.getAuthor().getAsTag()),
+            Component.text(":"),
+            Component.space(),
+            Component.text(event.getMessage().getContentDisplay(), NamedTextColor.GREEN)
+        ));
+    }
+
+    @EventHandler
+    public void onChat(AsyncChatEvent event){
+        TextChannel channel = Main.getJDA().getTextChannelById(823229253637373962L);
+        if(channel == null){
+            return;
+        }
+        channel.sendMessage("**" + event.getPlayer().getName() + "**: " + PlainComponentSerializer.plain().serialize(event.message())).queue();
+    }
+
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event){
+        TextChannel channel = Main.getJDA().getTextChannelById(823229253637373962L);
+        if(channel == null){
+            return;
+        }
+        channel.sendMessage("`" + event.getPlayer().getName() + "` がログインしました。").queue();
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event){
+        TextChannel channel = Main.getJDA().getTextChannelById(823229253637373962L);
+        if(channel == null){
+            return;
+        }
+        channel.sendMessage("`" + event.getPlayer().getName() + "` がログアウトしました。").queue();
+    }
+}
---
+++ src/main/java/org/jaoafa/gravel/Main.java
@@ -0,0 +1,58 @@
+package org.jaoafa.gravel;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public final class Main extends JavaPlugin {
+    static Main main;
+
+    static JDA jda;
+    @Override
+    public void onEnable() {
+        main = this;
+        getServer().sendMessage(Component.text().append(
+            Component.text("[Gravel]"),
+            Component.space(),
+            Component.text("プラグインが読み込まれました。", NamedTextColor.GREEN)
+        ));
+
+        FileConfiguration config = getConfig();
+        try {
+            JDABuilder jdabuilder = JDABuilder.createDefault(config.getString("token"))
+                // .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES)
+                .setAutoReconnect(true)
+                .setBulkDeleteSplittingEnabled(false)
+                .setContextEnabled(false)
+                .setEventManager(new AnnotatedEventManager());
+
+            jdabuilder.addEventListeners(new Event_ReceivedServerChat());
+
+            jda = jdabuilder.build().awaitReady();
+        } catch (Exception e) {
+            getLogger().warning("Discordへの接続に失敗しました。(" + e.getClass().getName() + " " + e.getMessage() + ")");
+            getLogger().warning("プラグインを無効化します。");
+            e.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        Objects.requireNonNull(getCommand("loadplugin")).setExecutor(new Cmd_LoadPlugin());
+        getServer().getPluginManager().registerEvents(new Event_ReceivedServerChat(), this);
+    }
+
+    public static JavaPlugin getJavaPlugin() {
+        return main;
+    }
+
+    public static JDA getJDA() {
+        return jda;
+    }
+}
